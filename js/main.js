528b1374743c19cad9f1a9c026a944708e68920a
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-10 23:50:51 +0000

    api works. working on css of results

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
-// var $searchBar = document.querySelector('.search-bar');
-// var $searchButton = document.querySelector('.search-button');
-// var $searchResults = document.querySelector('#search-results');
+var $searchInput = document.querySelector('#search-input');
+var $searchButton = document.querySelector('#search-button');
+var $searchResults = document.querySelector('#search-results');
 
-// $searchButton.addEventListener('click', showResults);
+$searchButton.addEventListener('click', showResults);
 
-// function showResults(event) {
-
-// }
+function showResults(event) {
+  $searchResults.replaceChildren('');
+  data.resultId = 1;
+  event.preventDefault();
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=' + $searchInput.value);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    if (xhr.status === 200) {
+      for (let i = 0; i < this.response.data[0].card_images.length; i++) {
+        var div = document.createElement('div');
+        div.setAttribute('class', 'column-fifth card-wrapper');
+        div.setAttribute('data-result-id', data.resultId);
+        data.resultId++;
+        var img = document.createElement('img');
+        img.setAttribute('class', 'card');
+        img.setAttribute('src', this.response.data[0].card_images[i].image_url);
+        img.setAttribute('alt', this.response.data[0].name);
+        div.appendChild(img);
+        $searchResults.appendChild(div);
+      }
+    } else if (xhr.status === 400) {
+      var noCard = document.createElement('div');
+      noCard.setAttribute('class', 'row');
+      var h1 = document.createElement('h1');
+      h1.setAttribute('class', 'center-text no-card-found');
+      var noCardFoundText = document.createTextNode('No Card Found');
+      h1.appendChild(noCardFoundText);
+      noCard.appendChild(h1);
+      $searchResults.appendChild(noCard);
+    }
+  });
+  xhr.send();
+}
