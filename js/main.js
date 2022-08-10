c8626038042cf13e9b111815e47956e5d9cb341c
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-10 20:23:54 +0000

    working on js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// var $searchBar = document.querySelector('.search-bar');
+// var $searchButton = document.querySelector('.search-button');
+// var $searchResults = document.querySelector('#search-results');
+
+// $searchButton.addEventListener('click', showResults);
+
+// function showResults(event) {
+
+// }
