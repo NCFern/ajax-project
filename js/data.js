505c9ce5dba21aa386ad290ca2192e31b4379c09
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-19 05:19:59 +0000

    modified data.js

--- js/data.js
+++ js/data.js
@@ -2,5 +2,21 @@
 
 var data = {
   view: 'search',
+  storedCards: {
+    cards: [],
+    nextCardId: 0
+  },
   resultId: 1
 };
+
+var storedEntries = localStorage.getItem('storedEntries');
+
+if (storedEntries !== null) {
+  data = JSON.parse(storedEntries);
+}
+
+window.addEventListener('beforeunload', saveEntries);
+
+function saveEntries(event) {
+  localStorage.setItem('storedEntries', JSON.stringify(data));
+}
